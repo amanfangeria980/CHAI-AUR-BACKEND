3d09796e42705079e5e793ac9aac5e06814a091c
amanfangeria980 <amanfangeria980@gmail.com>
2024-02-07 01:58:26 +0530

    Video 2 - Connecting frontend and backend project, cors and proxy

--- Day-1/frontend/src/App.jsx
+++ Day-1/frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <p>Jokes: {jokes.length}</p>
       {
         jokes.map((joke)=>{
-          <div key={joke.id}>
+          return <div key={joke.id}>
             <h3>{joke.title}</h3>
             <p>{joke.content}</p>
           </div>
